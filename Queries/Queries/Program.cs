f18c34708d50a46135418b23bbf228461676843b
luis1987 <luis.padilla@sistemabea.mx>
2017-08-25 18:24:37 -0500

    Queries project

---
+++ Queries/Queries/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            /*var query =
+                from c in context.Courses
+                where c.Name.Contains("c#");*/
+        }
+    }
+}
